ff45a7871ec09d09cffc6b97767687c20b977430
Justine Dela Torre <97047918+Jasutinn@users.noreply.github.com>
2023-10-12 07:49:33 +0800

    Create loop_str.java

---
+++ loop_str.java
@@ -0,0 +1,15 @@
+public class Jasutin {
+  // declare the main method
+  public static void main(String[] args) {
+    // declare a string variable
+    String str = "Justine Dela Torre";
+
+    for (int i = 0; i < str.length(); i++) {
+      // get the character at the current index
+      char ch = str.charAt(i);
+      // print the character
+      System.out.print(ch + " ");
+    }
+    // output: Justine Dela Torre
+  }
+}
